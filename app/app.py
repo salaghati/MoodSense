3e553734ded5e78fbd859a34028255322e8fbf17
Tú Trịnh <91258792+salaghati@users.noreply.github.com>
2025-04-01 14:31:59 +0700

    update ava

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 
 st.set_page_config(page_title="MoodSense by Tu", page_icon="🧠", layout="centered")
 
-st.image("https://imgur.com/a/JZADME3", width=120)
+st.image("hhttps://i.imgur.com/QzloMmO.png", width=120)
 st.markdown("## 👋 Hi! How are you feeling today?")
 
 
